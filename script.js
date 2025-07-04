cea1997546ee54a3417ec6c31f2df27466c84459
leandra001 <leanddrasilva3667@gmail.com>
2025-07-03 21:40:50 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("actionBtn");
+const msg = document.getElementById("message");
+
+btn.addEventListener("click", () => {msg.textContent =
+        "Quando você não pensa em desistir, o sucesso é inevitável.";});
